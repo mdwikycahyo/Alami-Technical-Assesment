afdf11edbe9b4b245fd57e9dd084ce3d04d836e6
Muhammad Dwiky Cahyo Wicaksono <mdwikycahyo@gmail.com>
2023-11-28 05:19:21 +0700

    using hash route

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
 import WAChatbot from "./pages/projects/wa-chatbot/index.tsx";
 
 import "./index.css";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
