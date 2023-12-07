bc7578aca4bfbaf9e4a320a41ed9468182f4a90a
Muhammad Dwiky Cahyo Wicaksono <mdwikycahyo@gmail.com>
2023-12-07 15:53:35 +0700

    implement google analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createHashRouter, RouterProvider } from "react-router-dom";
+import TagManager from 'react-gtm-module'
 import App from "./App.tsx";
 import WAChatbot from "./pages/projects/wa-chatbot/index.tsx";
 
 import "./index.css";
+
+const tagManagerArgs = {
+  gtmId: process.env.REACT_APP_GTM_ID || "",
+}
+
+TagManager.initialize(tagManagerArgs)
 
 const router = createHashRouter([
   {
